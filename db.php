73eb1c477a4acc163d7877ba6843cc41df747394
Snorre Gjøby <snorre_gjoby@hotmail.com>
2017-01-03 13:01:01 +0100

    added db connection

---
+++ db.php
@@ -0,0 +1,18 @@
+<?php
+
+
+try 							// Attempt a connection to the database:
+{
+	//$db = new PDO('mysql:host=127.0.0.1','root','');
+	$db = new PDO("mysql:host=eu-cdbr-west-01.cleardb.com;dbname=heroku_8ecd2cc03a114b6", "b273326046f12f", "26ff9f31");
+}
+catch (PDOException $e)			// If an error is detected
+{
+	if (isset($debug))			// If we are doing development
+		die ('Unable to connect to database : '.$e->getMessage());
+
+	else 						// Do NOT show above information to end users.
+		die ('Unable to connect to database, please try again later');
+}
+include_once("createDB.php");
+?>
